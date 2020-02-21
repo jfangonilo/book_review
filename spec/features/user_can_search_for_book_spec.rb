447c81593183b415b58229fcb2221533c1c65ddd
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-21 09:35:56 -0700

    Update spec

--- spec/features/user_can_search_for_book_spec.rb
+++ spec/features/user_can_search_for_book_spec.rb
@@ -1,12 +1,14 @@
 require 'rails_helper'
 
 describe 'User can search for a book' do
-  it 'shows results for the man who saw everything' do
+  it 'and shows results for the man who saw everything' do
     visit '/'
 
     fill_in :title, with: "the man who saw everything"
     click_on 'Find Book'
 
     expect(current_path).to eq '/search'
+    expect(page).to have_content "The Man Who Saw Everything"
+    expect(page).to have_content "which was longlisted for the Booker Prize, looks at masculinity through the perspective of a young historian who sneers at"
   end
 end
