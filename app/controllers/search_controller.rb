acc667ae0f1156c0591215a5814b048a57919b58
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-21 10:03:26 -0700

    Add nyt api quary

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -8,6 +8,11 @@ class SearchController < ApplicationController
     result = JSON.parse(response.body, symbolize_names: true)
     result_title = result[:docs].first[:title]
 
-
+    nyt_conn = Faraday.new('https://api.nytimes.com') do |f|
+      f.adapter Faraday.default_adapter
+    end
+    nyt_resp = nyt_conn.get("/svc/books/v3/reviews.json?api-key=#{ENV['NY_TIMES_API_KEY']}&title=#{result_title}")
+    nyt_result = JSON.parse(nyt_resp.body, symbolize_names: true)
+    result_summary = nyt_result[:results].first[:summary]
   end
 end
