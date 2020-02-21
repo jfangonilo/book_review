ccc488588fb80f2fb85ac0098992ac5ce410c754
Jomah Fangonilo <jomah.fangonilo@gmail.com>
2020-02-21 09:54:02 -0700

    Add openlibrary api call

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,13 @@
 class SearchController < ApplicationController
   def index
+    title = params[:title]
+    conn = Faraday.new('http://openlibrary.org') do |f|
+      f.adapter Faraday.default_adapter
+    end
+    response = conn.get("/search.json?q=#{title}")
+    result = JSON.parse(response.body, symbolize_names: true)
+    result_title = result[:docs].first[:title]
 
-    binding.pry
+
   end
 end
